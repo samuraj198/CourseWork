21c71f96cf35e87bc31c59e6a787838dae741fb4
samuraj198 <daniil885@mail.ru>
2025-03-30 20:35:31 +0400

    add redirect /index to /

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ use \App\Http\Controllers\IndexController;
 use \App\Http\Controllers\CatalogController;
 
 Route::get('/', [IndexController::class, 'index'])->name('index');
+Route::get('/index', function () {
+   return redirect('/');
+});
 
 Route::post('createCategory', [CategoriesController::class, 'store'])->name('createCategory');
 Route::post('createWork', [FilesController::class, 'store'])->name('createWork');
