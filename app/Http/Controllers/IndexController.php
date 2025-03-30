666c3ddf26735126c07bcaa43340d2097a1fe0df
samuraj198 <daniil885@mail.ru>
2025-03-30 14:23:59 +0400

    change index view in controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -15,7 +15,7 @@ class IndexController extends Controller
     {
         $categories = Category::orderBy('created_at', 'desc')->take(6)->get();
         $works = File::orderBy('created_at', 'desc')->take(6)->get();
-        return view('/', compact('categories', 'works'));
+        return redirect()->route('index', compact('categories', 'works'));
     }
 
     /**
